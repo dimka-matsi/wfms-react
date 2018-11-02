32217c87172ad6b66319f84f6754b9e716016759
Dima Matskello <dimka_matsi@hotmail.com>
2018-11-02 20:27:16 +0300

    change car.js

--- src/Car/car.js
+++ src/Car/car.js
@@ -2,42 +2,48 @@ import React from 'react';
 import Radium from 'radium';
 import classes from './Car.css';
 
-const Car = (props) => {
-	const inputClasses = [classes.input];
+class Car extends React.Component {
+	render() {
+		const inputClasses = [classes.input];
 
-	if (props.name !== '') {
-		inputClasses.push(classes.green);
-	} else {
-		inputClasses.push(classes.red);
-	}
-
-	if(props.name.length > 4) {
-		inputClasses.push(classes.bold);
-	}
-
-	const style = {
-		border: '1px solid #ccc',
-		boxShadow: '0 4px 5px rgba(0,0,0,.14)',
-		':hover': {
-			border: '1px solid #aaa',
-			boxShadow: '0 4px 15px 0 rgba(0,0,0, ,25)',
-			cursor: 'pointer'
+		if (this.props.name !== '') {
+			inputClasses.push(classes.green);
+		} else {
+			inputClasses.push(classes.red);
 		}
-	}
 
-	return (
-		<div className={classes.Car} style={style}>
-			<h3>Car name: {props.name}</h3>
-			<p>Year: <strong>{props.year}</strong></p>
-			<input
-				type="text"
-				onChange={props.onChangeName}
-				value={props.name}
-				className={inputClasses.join(' ')}
-			/>
-			<button onClick={props.onDelete}>Delete</button>
-		</div>
-	);
+		if(this.props.name.length > 4) {
+			inputClasses.push(classes.bold);
+		}
+
+		const style = {
+			border: '1px solid #ccc',
+			boxShadow: '0 4px 5px rgba(0,0,0,.14)',
+			':hover': {
+				border: '1px solid #aaa',
+				boxShadow: '0 4px 15px 0 rgba(0,0,0, ,25)',
+				cursor: 'pointer'
+			}
+		}
+
+		return (
+			<div className={classes.Car} style={style}>
+				<h3>Car name: {this.props.name}</h3>
+				<p>Year: <strong>{this.props.year}</strong></p>
+				<input
+					type="text"
+					onChange={this.props.onChangeName}
+					value={this.props.name}
+					className={inputClasses.join(' ')}
+				/>
+				<button onClick={this.props.onDelete}>Delete</button>
+			</div>
+		);
+	}
 }
+
+// const Car = (props) => {
+	
+// }
 
 export default Radium(Car);
